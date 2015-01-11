31ce0fb521e3cb1b3e9a89f4d426d38b67429676
Joshua <stroebele@gmail.com>
2015-01-11 12:19:39 -0600

    Initial Auth

--- StarterKit/App_Start/WebApiConfig.cs
+++ StarterKit/App_Start/WebApiConfig.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace StarterKit
 {
@@ -9,8 +10,6 @@ namespace StarterKit
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +18,9 @@ namespace StarterKit
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
